0d81d9e375e463448aed36f11596098f615d3737
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-07-08 22:35:19 +0300

    new service - rts-mux-server

--- proto_compile.sh
+++ proto_compile.sh
@@ -6,7 +6,8 @@ rm -f rpc/rts-server/iot_realtime/*.pb.go
 rm -f nodemcu-client/*.pb.*
 rm -f nodemcu-client/pb_*.{h,c}
 
-protoc -I/usr/local/include -Iproto/ --go_out=plugins=grpc:rpc/rts-server/iot_realtime proto/time_service.proto
+protoc -I/usr/local/include -Iproto/ --go_out=plugins=grpc:rpc/rts-grpc-server/iot_realtime proto/time_service.proto
+protoc -I/usr/local/include -Iproto/ --go_out=plugins=grpc:rpc/rts-mux-server/iot_realtime proto/time_service.proto
 #python3 -m grpc_tools.protoc -Iproto/ --python_out=nodemcu-client --grpc_python_out=nodemcu-client proto/*.proto
 python3 nanopb/generator/nanopb_generator.py -Iproto/ -Dnodemcu-client/ proto/*.proto
 
